2b79586def4dda65857a93148cfe02830ba1dcdf
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-08-17 19:17:27 +0300

    Started

---
+++ src/components/Container/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './Container.scss';
+import './Container.css';
+
+
+const Container = ({ children }) => <div className="Container">{children}</div>;
+
+export default Container;
